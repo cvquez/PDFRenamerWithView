d0bf9cd2cab89d5760d4432d7dabef231d4993ec
cristhian.velazquez <cristhian.velazquez@bigbox.com.py>
2022-02-16 15:45:03 -0300

    application's entry-point script

---
+++ pdfrenamer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pdfrenamer.py
+
+"""This module provides the RP Renamer entry-point script."""
+
+from pdfrenamer.app import main
+
+if __name__ == "__main__":
+    main()
